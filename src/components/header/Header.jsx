7af49cc4e4a2101ba259494822d2f593d8f865dc
Lagomar18 <lagomarsino.epeim@gmail.com>
2024-06-04 12:59:53 -0300

    modificación

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -7,7 +7,7 @@ export const Header = (props) => {
 
   return (
     <header className="header">
-        <Link ><h1>LagoShop</h1></Link>
+        <Link ><h1>LoremStore</h1></Link>
         <NavBar />
         <Carrito numerito={props.numerito} />
     </header>
